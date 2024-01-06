1727435686291b106228bfb08be582cdfcb872ff
rakesh tekam <rakeshtekam280@gmail.com>
2024-01-06 23:56:57 +0530

    user

--- init/index.js
+++ init/index.js
@@ -19,7 +19,7 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
-  initData.data=initData.data.map((obj)=>({...obj, owner:""}))
+  initData.data=initData.data.map((obj)=>({...obj, owner:"65999b31101320ca8ddbe662"}))
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
