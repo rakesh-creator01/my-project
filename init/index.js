23609225a30aa0f7fb1d0faabc009663c1a3bc3c
rakesh tekam <rakeshtekam280@gmail.com>
2024-01-06 23:38:26 +0530

    url error

--- init/index.js
+++ init/index.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-// const MONGO_URL = "mongodb://127.0.0.1:27017/LANDSELL";
-const dbUrl = process.env.ATLASDB_URL;
+const MONGO_URL = "mongodb://127.0.0.1:27017/LANDSELL";
+// const dbUrl = process.env.ATLASDB_URL;
 
 main()
   .then(() => {
@@ -14,7 +14,7 @@ main()
   });
 
 async function main() {
-  await mongoose.connect(dbUrl);
+  await mongoose.connect(MONGO_URL);
 }
 
 const initDB = async () => {
