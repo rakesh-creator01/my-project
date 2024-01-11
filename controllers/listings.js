48204481af02989b548bad9cb6467bebdbad0688
rakesh tekam <rakeshtekam280@gmail.com>
2024-01-11 22:00:15 +0530

    orignal listing

--- controllers/listings.js
+++ controllers/listings.js
@@ -3,7 +3,7 @@ const Listing =require("../models/listing");
 
 module.exports.index =async (req, res) => {
     const allListings = await Listing.find({});
-    res.render("./listings/index.ejs", { allListings });
+    res.render("listings/index.ejs", { allListings });
   };
 
 
